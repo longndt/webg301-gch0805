b3214792dbf94209e1818bfdff6d527ac1aabbaa
longndt <55429399+longndt@users.noreply.github.com>
2021-12-04 13:37:35 +0700

    ok

--- person/var/cache/dev/twig/20/205baac5d4d397eb08122ef44373fadb29254533ea1b320fa6ba53844127a9a1.php
+++ person/var/cache/dev/twig/20/205baac5d4d397eb08122ef44373fadb29254533ea1b320fa6ba53844127a9a1.php
@@ -30,6 +30,7 @@ class __TwigTemplate_870e38572bde2ce2d997dd3da5197bf3de751d567d12ffed0dd3067568b
             'title' => [$this, 'block_title'],
             'stylesheets' => [$this, 'block_stylesheets'],
             'javascripts' => [$this, 'block_javascripts'],
+            'nav' => [$this, 'block_nav'],
             'body' => [$this, 'block_body'],
         ];
     }
@@ -56,18 +57,21 @@ class __TwigTemplate_870e38572bde2ce2d997dd3da5197bf3de751d567d12ffed0dd3067568b
         // line 8
         echo "        ";
         $this->displayBlock('stylesheets', $context, $blocks);
-        // line 13
+        // line 14
         echo "
         ";
-        // line 14
+        // line 15
         $this->displayBlock('javascripts', $context, $blocks);
-        // line 17
+        // line 18
         echo "    </head>
     <body>
         ";
-        // line 19
-        $this->displayBlock('body', $context, $blocks);
         // line 20
+        $this->displayBlock('nav', $context, $blocks);
+        // line 43
+        echo "        ";
+        $this->displayBlock('body', $context, $blocks);
+        // line 44
         echo "    </body>
 </html>
 ";
@@ -110,11 +114,12 @@ class __TwigTemplate_870e38572bde2ce2d997dd3da5197bf3de751d567d12ffed0dd3067568b
 
         // line 9
         echo "           <!-- CSS only -->
-            <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3\" crossorigin=\"anonymous\">
-            <link rel=\"stylesheet\" href=\"";
-        // line 11
+                       <link rel=\"stylesheet\" href=\"";
+        // line 10
         echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("css/style.css"), "html", null, true);
         echo "\">
+            <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3\" crossorigin=\"anonymous\">
+
         ";
         
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
@@ -124,7 +129,7 @@ class __TwigTemplate_870e38572bde2ce2d997dd3da5197bf3de751d567d12ffed0dd3067568b
 
     }
 
-    // line 14
+    // line 15
     public function block_javascripts($context, array $blocks = [])
     {
         $macros = $this->macros;
@@ -134,9 +139,9 @@ class __TwigTemplate_870e38572bde2ce2d997dd3da5197bf3de751d567d12ffed0dd3067568b
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascripts"));
 
-        // line 15
-        echo "            ";
         // line 16
+        echo "            ";
+        // line 17
         echo "        ";
         
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
@@ -146,7 +151,57 @@ class __TwigTemplate_870e38572bde2ce2d997dd3da5197bf3de751d567d12ffed0dd3067568b
 
     }
 
-    // line 19
+    // line 20
+    public function block_nav($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "nav"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "nav"));
+
+        // line 21
+        echo "            <nav class=\"navbar navbar-expand-lg navbar-light bg-light\">
+                <div class=\"container-fluid\">
+                    <ul class=\"navbar-nav\">
+                        <h2>
+                            <li class=\"nav-item\">
+                                <a class=\"nav-link text-uppercase\" href=\"";
+        // line 26
+        echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("person_index");
+        echo "\">Person</a>
+                            </li>
+                        </h2>
+                        <h2>
+                            <li class=\"nav-item\">
+                                <a class=\"nav-link text-uppercase\" href=\"";
+        // line 31
+        echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("job_index");
+        echo "\">Job</a>
+                            </li>
+                        </h2>
+                        <h2>
+                            <li class=\"nav-item\">
+                                <a class=\"nav-link text-uppercase\" href=\"";
+        // line 36
+        echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("car_index");
+        echo "\">Car</a>
+                            </li>
+                        </h2>  
+                    </ul>
+                </div>
+            </nav>
+        ";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 43
     public function block_body($context, array $blocks = [])
     {
         $macros = $this->macros;
@@ -171,7 +226,7 @@ class __TwigTemplate_870e38572bde2ce2d997dd3da5197bf3de751d567d12ffed0dd3067568b
 
     public function getDebugInfo()
     {
-        return array (  150 => 19,  140 => 16,  138 => 15,  128 => 14,  116 => 11,  112 => 9,  102 => 8,  83 => 5,  71 => 20,  69 => 19,  65 => 17,  63 => 14,  60 => 13,  57 => 8,  53 => 5,  47 => 1,);
+        return array (  205 => 43,  188 => 36,  180 => 31,  172 => 26,  165 => 21,  155 => 20,  145 => 17,  143 => 16,  133 => 15,  119 => 10,  116 => 9,  106 => 8,  87 => 5,  75 => 44,  72 => 43,  70 => 20,  66 => 18,  64 => 15,  61 => 14,  58 => 8,  54 => 5,  48 => 1,);
     }
 
     public function getSourceContext()
@@ -185,8 +240,9 @@ class __TwigTemplate_870e38572bde2ce2d997dd3da5197bf3de751d567d12ffed0dd3067568b
            and uncomment the following Encore helpers to start using Symfony UX #}
         {% block stylesheets %}
            <!-- CSS only -->
+                       <link rel=\"stylesheet\" href=\"{{ asset('css/style.css')}}\">
             <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3\" crossorigin=\"anonymous\">
-            <link rel=\"stylesheet\" href=\"{{ asset('css/style.css')}}\">
+
         {% endblock %}
 
         {% block javascripts %}
@@ -194,6 +250,29 @@ class __TwigTemplate_870e38572bde2ce2d997dd3da5197bf3de751d567d12ffed0dd3067568b
         {% endblock %}
     </head>
     <body>
+        {% block nav %}
+            <nav class=\"navbar navbar-expand-lg navbar-light bg-light\">
+                <div class=\"container-fluid\">
+                    <ul class=\"navbar-nav\">
+                        <h2>
+                            <li class=\"nav-item\">
+                                <a class=\"nav-link text-uppercase\" href=\"{{ path('person_index') }}\">Person</a>
+                            </li>
+                        </h2>
+                        <h2>
+                            <li class=\"nav-item\">
+                                <a class=\"nav-link text-uppercase\" href=\"{{ path('job_index') }}\">Job</a>
+                            </li>
+                        </h2>
+                        <h2>
+                            <li class=\"nav-item\">
+                                <a class=\"nav-link text-uppercase\" href=\"{{ path('car_index') }}\">Car</a>
+                            </li>
+                        </h2>  
+                    </ul>
+                </div>
+            </nav>
+        {% endblock %}
         {% block body %}{% endblock %}
     </body>
 </html>
