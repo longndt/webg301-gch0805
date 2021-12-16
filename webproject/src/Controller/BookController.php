bc3a7eee55540608d5f826fa5506434ba6eec20a
longndt <55429399+longndt@users.noreply.github.com>
2021-12-16 17:13:42 +0700

    ok

--- webproject/src/Controller/BookController.php
+++ webproject/src/Controller/BookController.php
@@ -37,14 +37,31 @@ class BookController extends AbstractController
      * @Route("/book/{id}", name="book_detail")
      */
     public function bookDetail($id) {
-
+        $book = $this->getDoctrine()->getRepository(Book::class)->find($id);
+        if ($book == null) {
+            $this->addFlash("Error","Book not found");
+            return $this->redirectToRoute("book_index");
+        }
+        return $this->render("book/detail.html.twig",
+        [
+            "book" => $book
+        ]);
     }
 
     /**
      * @Route("/book/delete/{id}", name="book_delete")
      */
     public function bookDelete($id) {
-
+        $book = $this->getDoctrine()->getRepository(Book::class)->find($id);
+        if ($book == null) {
+            $this->addFlash("Error","Delete book failed");
+        } else {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->remove($book);
+            $manager->flush();
+            $this->addFlash("Success","Delete book succeed");
+        }
+        return $this->redirectToRoute("book_index");
     }
 
     /**
@@ -58,6 +75,6 @@ class BookController extends AbstractController
      * @Route("/book/edit/{id}", name="book_edit")
      */
     public function bookEdit(Request $request, $id) {
-        
+
     }
 }
